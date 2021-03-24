ceb08e5cd7c5849ea23eb2f411ca20be15a0a0bd
kteneta <kteneta>
2021-03-24 16:26:59 +0200

    fixed reading message from s3

--- s3-to-sqs/src/handler.py
+++ s3-to-sqs/src/handler.py
@@ -1,7 +1,7 @@
 import json
 import logging
 import os
-
+import sys
 import boto3
 
 logger = logging.getLogger()
@@ -12,39 +12,36 @@ SQS = boto3.client('sqs')
 
 
 def read_file_from_s3(event):
-    s3 = boto3.client('s3')
-    bucketname = event['Records'][0]['s3']['bucket']['name']
-    itemname = event['Records'][0]['s3']['object']['key']
-    with open('FILE_NAME', 'wb') as f:
-        s3.download_fileobj(bucketname, itemname, f)
-        print(f)
-    
-    
+    s3 = boto3.resource('s3')
+    message = []
+    for record in event['Records']:
+        bucket = record['s3']['bucket']['name']
+        key = record['s3']['object']['key']
+        obj = s3.Object(bucket, key)
+        base = obj.get()['Body'].read().decode('utf8')
+        message.append(base)
+    return message
 
-
-    # obj = s3.Object(bucketname, itemname)
-    # body = obj.get()['Body'].read()
-    # print(body)
-    os.exit(0)
 
 def producer(event, context):
-    read_file_from_s3(event)
+    queue_message = read_file_from_s3(event)
     status_code = 200
     message = ''
 
-    if not event['body']:
-        return {'statusCode': 400, 'body': json.dumps({'message': 'No body was found'})}
+    # if not event['body']:
+    #     return {'statusCode': 400, 'body': json.dumps({'message': 'No body was found'})}
 
     try:
         message_attrs = {
             'AttributeName': {'StringValue': 'AttributeValue', 'DataType': 'String'}
         }
-        SQS.send_message(
-            QueueUrl=QUEUE_URL,
-            MessageBody=event['body'],
-            MessageAttributes=message_attrs,
-        )
-        message = 'Message accepted!'
+        for message in queue_message:
+            SQS.send_message(
+                QueueUrl=QUEUE_URL,
+                MessageBody=message,
+                MessageAttributes=message_attrs,
+            )
+            message = 'Message accepted!'
     except Exception as e:
         logger.exception('Sending message to SQS queue failed!')
         message = str(e)
