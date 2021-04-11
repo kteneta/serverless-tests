38fd85812950530ffb4251ce52fa5571ed32cd79
kteneta <kteneta>
2021-04-11 23:06:55 +0300

    added base files for cdk app

---
+++ cdk-app/app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import os
+
+from aws_cdk import core as cdk
+
+from aws_cdk import core
+
+from cdk_app.cdk_app_stack import CdkAppStack
+
+
+app = core.App()
+CdkAppStack(app, "MyTestStack")
+
+app.synth()
